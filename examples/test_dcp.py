47b3c69b2eebeda4ea99f8d993937cdc790be507
Mark Brightman <mark@solutions.to>
2021-05-06 19:13:26 +0100

    using plotly to replace o3d viz - WIP

--- examples/test_dcp.py
+++ examples/test_dcp.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 import torch.utils.data
 import torchvision
+import plotly.graph_objects as go
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
@@ -27,6 +28,51 @@ def get_transformations(igt):
 	R_ab = R_ba.permute(0, 2, 1)                        # Pt = R_ab * Ps
 	translation_ab = -torch.bmm(R_ab, translation_ba)   # Pt = Ps + t_ab
 	return R_ab, translation_ab, R_ba, translation_ba
+
+def draw_geometries(geometries):
+    graph_objects = []
+
+    for geometry in geometries:
+        geometry_type = geometry.get_geometry_type()
+        
+        if geometry_type == o3d.geometry.Geometry.Type.PointCloud:
+            points = np.asarray(geometry.points)
+            colors = None
+            if geometry.has_colors():
+                colors = np.asarray(geometry.colors)
+            elif geometry.has_normals():
+                colors = (0.5, 0.5, 0.5) + np.asarray(geometry.normals) * 0.5
+            else:
+                geometry.paint_uniform_color((1.0, 0.0, 0.0))
+                colors = np.asarray(geometry.colors)
+
+            scatter_3d = go.Scatter3d(x=points[:,0], y=points[:,1], z=points[:,2], mode='markers', marker=dict(size=1, color=colors))
+            graph_objects.append(scatter_3d)
+
+        if geometry_type == o3d.geometry.Geometry.Type.TriangleMesh:
+            triangles = np.asarray(geometry.triangles)
+            vertices = np.asarray(geometry.vertices)
+            colors = None
+            if geometry.has_triangle_normals():
+                colors = (0.5, 0.5, 0.5) + np.asarray(geometry.triangle_normals) * 0.5
+                colors = tuple(map(tuple, colors))
+            else:
+                colors = (1.0, 0.0, 0.0)
+            
+            mesh_3d = go.Mesh3d(x=vertices[:,0], y=vertices[:,1], z=vertices[:,2], i=triangles[:,0], j=triangles[:,1], k=triangles[:,2], facecolor=colors, opacity=0.50)
+            graph_objects.append(mesh_3d)
+        
+    fig = go.Figure(
+        data=graph_objects,
+        layout=dict(
+            scene=dict(
+                xaxis=dict(visible=False),
+                yaxis=dict(visible=False),
+                zaxis=dict(visible=False)
+            )
+        )
+    )
+    fig.show()
 
 def display_open3d(template, source, transformed_source):
 	template_ = o3d.geometry.PointCloud()
@@ -123,6 +169,8 @@ def main():
 		args.device = 'cpu'
 	args.device = torch.device(args.device)
 
+	o3d.visualization.draw_geometries = draw_geometries
+	
 	# Create PointNet Model.
 	dgcnn = DGCNN(emb_dims=args.emb_dims)
 	model = DCP(feature_model=dgcnn, cycle=True)
--- examples/test_rpmnet.py
+++ examples/test_rpmnet.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 import torch.utils.data
 import torchvision
+import plotly.graph_objects as go
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
@@ -21,6 +22,51 @@ if BASE_DIR[-8:] == 'examples':
 from learning3d.models import RPMNet, PPFNet
 from learning3d.losses import FrobeniusNormLoss, RMSEFeaturesLoss
 from learning3d.data_utils import RegistrationData, ModelNet40Data
+
+def draw_geometries(geometries):
+    graph_objects = []
+
+    for geometry in geometries:
+        geometry_type = geometry.get_geometry_type()
+        
+        if geometry_type == o3d.geometry.Geometry.Type.PointCloud:
+            points = np.asarray(geometry.points)
+            colors = None
+            if geometry.has_colors():
+                colors = np.asarray(geometry.colors)
+            elif geometry.has_normals():
+                colors = (0.5, 0.5, 0.5) + np.asarray(geometry.normals) * 0.5
+            else:
+                geometry.paint_uniform_color((1.0, 0.0, 0.0))
+                colors = np.asarray(geometry.colors)
+
+            scatter_3d = go.Scatter3d(x=points[:,0], y=points[:,1], z=points[:,2], mode='markers', marker=dict(size=1, color=colors))
+            graph_objects.append(scatter_3d)
+
+        if geometry_type == o3d.geometry.Geometry.Type.TriangleMesh:
+            triangles = np.asarray(geometry.triangles)
+            vertices = np.asarray(geometry.vertices)
+            colors = None
+            if geometry.has_triangle_normals():
+                colors = (0.5, 0.5, 0.5) + np.asarray(geometry.triangle_normals) * 0.5
+                colors = tuple(map(tuple, colors))
+            else:
+                colors = (1.0, 0.0, 0.0)
+            
+            mesh_3d = go.Mesh3d(x=vertices[:,0], y=vertices[:,1], z=vertices[:,2], i=triangles[:,0], j=triangles[:,1], k=triangles[:,2], facecolor=colors, opacity=0.50)
+            graph_objects.append(mesh_3d)
+        
+    fig = go.Figure(
+        data=graph_objects,
+        layout=dict(
+            scene=dict(
+                xaxis=dict(visible=False),
+                yaxis=dict(visible=False),
+                zaxis=dict(visible=False)
+            )
+        )
+    )
+    fig.show()
 
 def display_open3d(template, source, transformed_source):
 	template_ = o3d.geometry.PointCloud()
@@ -104,6 +150,8 @@ def main():
 	if not torch.cuda.is_available():
 		args.device = 'cpu'
 	args.device = torch.device(args.device)
+	
+	o3d.visualization.draw_geometries = draw_geometries
 
 	# Create RPMNet Model.
 	model = RPMNet(feature_model=PPFNet())
