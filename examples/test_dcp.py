a0651efd2089de93b39b070725d6db5c0e89a07d
Mark Brightman <mark@solutions.to>
2021-05-13 13:37:52 +0100

    plot_geometries

--- examples/test_dcp.py
+++ examples/test_dcp.py
@@ -30,51 +30,6 @@ def get_transformations(igt):
 	translation_ab = -torch.bmm(R_ab, translation_ba)   # Pt = Ps + t_ab
 	return R_ab, translation_ab, R_ba, translation_ba
 
-def draw_geometries(geometries):
-    graph_objects = []
-
-    for geometry in geometries:
-        geometry_type = geometry.get_geometry_type()
-        
-        if geometry_type == o3d.geometry.Geometry.Type.PointCloud:
-            points = np.asarray(geometry.points)
-            colors = None
-            if geometry.has_colors():
-                colors = np.asarray(geometry.colors)
-            elif geometry.has_normals():
-                colors = (0.5, 0.5, 0.5) + np.asarray(geometry.normals) * 0.5
-            else:
-                geometry.paint_uniform_color((1.0, 0.0, 0.0))
-                colors = np.asarray(geometry.colors)
-
-            scatter_3d = go.Scatter3d(x=points[:,0], y=points[:,1], z=points[:,2], mode='markers', marker=dict(size=1, color=colors))
-            graph_objects.append(scatter_3d)
-
-        if geometry_type == o3d.geometry.Geometry.Type.TriangleMesh:
-            triangles = np.asarray(geometry.triangles)
-            vertices = np.asarray(geometry.vertices)
-            colors = None
-            if geometry.has_triangle_normals():
-                colors = (0.5, 0.5, 0.5) + np.asarray(geometry.triangle_normals) * 0.5
-                colors = tuple(map(tuple, colors))
-            else:
-                colors = (1.0, 0.0, 0.0)
-            
-            mesh_3d = go.Mesh3d(x=vertices[:,0], y=vertices[:,1], z=vertices[:,2], i=triangles[:,0], j=triangles[:,1], k=triangles[:,2], facecolor=colors, opacity=0.50)
-            graph_objects.append(mesh_3d)
-        
-    fig = go.Figure(
-        data=graph_objects,
-        layout=dict(
-            scene=dict(
-                xaxis=dict(visible=False),
-                yaxis=dict(visible=False),
-                zaxis=dict(visible=False)
-            )
-        )
-    )
-    fig.show()
-
 def plot_geometries(label, geometries):
     plt.rcParams['figure.figsize'] = [16, 16]
     ax = plt.axes(projection='3d')
--- examples/test_pointnet.py
+++ examples/test_pointnet.py
@@ -23,68 +23,7 @@ if BASE_DIR[-8:] == 'examples':
 from learning3d.models import PointNet
 from learning3d.models import Classifier
 from learning3d.data_utils import ClassificationData, ModelNet40Data
-
-def draw_cloud(cloud):
-	points = np.asarray(cloud.points)
-	colors = None
-	if cloud.has_colors():
-    	colors = np.asarray(cloud.colors)
-	elif cloud.has_normals():
-    	colors = (0.5, 0.5, 0.5) + np.asarray(cloud.normals) * 0.5
-	else:
-    	geometry.paint_uniform_color((1.0, 0.0, 0.0))
-    	colors = np.asarray(geometry.colors)
-	ax = plt.axes(projection='3d')
-	ax.view_init(90, -90)
-	ax.axis("off")
-	ax.scatter(points[:,0], points[:,1], points[:,2], s=1, c=colors)
-	plt.show()
 	
-def draw_geometries(geometries):
-    graph_objects = []
-
-    for geometry in geometries:
-        geometry_type = geometry.get_geometry_type()
-        
-        if geometry_type == o3d.geometry.Geometry.Type.PointCloud:
-            points = np.asarray(geometry.points)
-            colors = None
-            if geometry.has_colors():
-                colors = np.asarray(geometry.colors)
-            elif geometry.has_normals():
-                colors = (0.5, 0.5, 0.5) + np.asarray(geometry.normals) * 0.5
-            else:
-                geometry.paint_uniform_color((1.0, 0.0, 0.0))
-                colors = np.asarray(geometry.colors)
-
-            scatter_3d = go.Scatter3d(x=points[:,0], y=points[:,1], z=points[:,2], mode='markers', marker=dict(size=1, color=colors))
-            graph_objects.append(scatter_3d)
-
-        if geometry_type == o3d.geometry.Geometry.Type.TriangleMesh:
-            triangles = np.asarray(geometry.triangles)
-            vertices = np.asarray(geometry.vertices)
-            colors = None
-            if geometry.has_triangle_normals():
-                colors = (0.5, 0.5, 0.5) + np.asarray(geometry.triangle_normals) * 0.5
-                colors = tuple(map(tuple, colors))
-            else:
-                colors = (1.0, 0.0, 0.0)
-            
-            mesh_3d = go.Mesh3d(x=vertices[:,0], y=vertices[:,1], z=vertices[:,2], i=triangles[:,0], j=triangles[:,1], k=triangles[:,2], facecolor=colors, opacity=0.50)
-            graph_objects.append(mesh_3d)
-        
-    fig = go.Figure(
-        data=graph_objects,
-        layout=dict(
-            scene=dict(
-                xaxis=dict(visible=False),
-                yaxis=dict(visible=False),
-                zaxis=dict(visible=False)
-            )
-        )
-    )
-    fig.show()
-
 def plot_geometries(label, geometries):
     plt.rcParams['figure.figsize'] = [16, 16]
     ax = plt.axes(projection='3d')
@@ -194,8 +133,6 @@ def main():
 	if not torch.cuda.is_available():
 		args.device = 'cpu'
 	args.device = torch.device(args.device)
-
-	o3d.visualization.draw_geometries = draw_geometries
 
 	# Create PointNet Model.
 	ptnet = PointNet(emb_dims=args.emb_dims, use_bn=True)
